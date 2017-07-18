bf24057012ec307a2148df76e681de5367433863
isaaclax <Isaac.Lax@us.qbe.com>
2017-07-18 14:54:18 -0500

    adding new

--- trial2.py
+++ trial2.py
@@ -1,4 +1,5 @@
 from sense_hat import SenseHat
+import csv
 sense = SenseHat()
 
 while True:
@@ -10,6 +11,11 @@ while True:
     p = round(p, 1)
     h = round(h, 1)
 
-    msg = "Temperature = {0}, Pressure = {1}, Humidity = {2}".format(t,p,h)
+with open(<data.csv>, "wb") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for line in data:
+            writer.writerow(line)
+            
+    # msg = "Temperature = {0}, Pressure = {1}, Humidity = {2}".format(t,p,h)
 
-    sense.show_message(msg, scroll_speed=0.05)
+    # sense.show_message(msg, scroll_speed=0.05)
