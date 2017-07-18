2ef2008f6e93faa186acbdaeb809b219fa7da30f
isaaclax <Isaac.Lax@us.qbe.com>
2017-07-18 14:49:41 -0500

    adding new

--- trial2.py
+++ trial2.py
@@ -1,35 +1,15 @@
-import keen
 from sense_hat import SenseHat
-from datetime import datetime
+sense = SenseHat()
 
-keen.project_id = "596e366ec9e77c00015d99e3"
-keen.write_key = "4C65FC4CAFC0EBF6BDD3919989C27C23A591DE227A92E8E1C45DBB8D7E84500EC6AACF57D8BC361877F923B8DC457B08C083BABAB80083FA67E14D7C3B355C1EB34FBE1E8D140543B608C08AEAA1737C9778C30159EE74351BE56327C5FD1F5C"
+while True:
+    t = sense.get_temperature()
+    p = sense.get_pressure()
+    h = sense.get_humidity()
 
-def record_data():
-    sense = SenseHat()
+    t = round(t, 1)
+    p = round(p, 1)
+    h = round(h, 1)
 
-    d = get_timestamp()
-    t = sense.temperature
-    h = sense.humidity
-    values = (d, t, h)
+    msg = "Temperature = {0}, Pressure = {1}, Humidity = {2}".format(t,p,h)
 
-keen.add_event("data", {
-  "device": "hodor",
-  "time_stamp": d,
-  "temperature": t,
-  "humidity": h
-})
-
-
-def get_timestamp():
-    dt = datetime.now()
-    dt_date = str(dt.date())
-    dt_time = str(dt.time())
-    timestamp = "%s %s" % (dt_date, dt_time[:8])
-    return timestamp
-
- def main():
-    record_data()
-
-if __name__ == '__main__':
-    main()
+    sense.show_message(msg, scroll_speed=0.05)
